aeea2efc7f09b8e51a41649fbe98b7adbb90bb34
InbalBu <inbalbu2@gmail.com>
2022-08-30 12:09:56 +0300

    added projects

--- src/Project.js
+++ src/Project.js
@@ -3,6 +3,9 @@ import "./Project.css";
 import project from './img/project.png'
 import project2 from './img/project2.png'
 import project3 from './img/project3.jpg'
+import project4 from './img/project4.png'
+import project5 from './img/project5.jpg'
+import project6 from './img/project6.png'
 
 function Project() {
   return (
@@ -26,7 +29,7 @@ function Project() {
                      </div>
                      <div className="project__meta absolute">
                      <h5 className="project__text">Todo List</h5>
-                     <h4 className="project__text"> Add, remove or check a todo. <br/> Programmed in React.</h4>
+                     <h4 className="project__text2"> Add, remove or check a todo. <br/> Programmed in React.</h4>
                      <a href="https://github.com/InbalBu/todolist"
                      className="project__btn">View Details</a>
                      </div>
@@ -43,7 +46,7 @@ function Project() {
                      </div>
                      <div className="project__meta absolute">
                      <h5 className="project__text">ECommerce Website</h5>
-                     <h4 className="project__text"> ECommerce web shop app <br/> using React.js.</h4>
+                     <h4 className="project__text2"> ECommerce web shop app <br/> using React.js.</h4>
                      <a href="https://github.com/InbalBu/ecommerce" className="project__btn">View Details</a>
                      </div>
                  </div>
@@ -59,8 +62,56 @@ function Project() {
                      </div>
                      <div className="project__meta absolute">
                      <h5 className="project__text">Library Managment System</h5>
-                     <h4 className="project__text"> Library manager in program  C#, <br/> WPF, using MVVM Architecture.</h4>
+                     <h4 className="project__text2"> Library manager in program  C#, <br/> WPF, using MVVM Architecture.</h4>
                      <a href="https://github.com/InbalBu/Library-Management-System" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+             <div className="col__2">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={project4} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Animal Pet Shop</h5>
+                     <h4 className="project__text2"> Pet Shop Programmed in ASP.Net, <br/> MVC.</h4>
+                     <a href="https://github.com/InbalBu/AnimalPetShop" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+             <div className="col__2">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={project5} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Recipe Blog</h5>
+                     <h4 className="project__text2">programmed in EJS, Express and Mongodb.</h4>
+                     <a href="https://github.com/InbalBu/recipeblog" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+             <div className="col__2">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={project6} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Shoe Store</h5>
+                     <h4 className="project__text2">Programmed in React, PayPal, sanity and MongoDB.</h4>
+                     <a href="https://github.com/InbalBu/EcommerceApp" className="project__btn">View Details</a>
                      </div>
                  </div>
              </div>
